c6090348e924ab340aa930d576c5bf1586b3a915
ssannccoss <sanjarbekalisherov837@gmail.com>
2024-10-06 13:43:50 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
+import 'package:expense_tracker_app/expenses.dart';
 import 'package:flutter/material.dart';
-
-import 'package:expense_tracker/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: ...,
+      home: Expenses(),
     ),
   );
 }
